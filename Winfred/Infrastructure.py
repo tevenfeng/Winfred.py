2f49cbb7d240c85b3d9385cbe89fb9cd0b9b4477
Teven Feng <fengdingwen@outlook.com>
2021-12-30 22:56:42 +0800

    add pynput

--- Winfred/Infrastructure.py
+++ Winfred/Infrastructure.py
@@ -26,22 +26,27 @@ def detect_os():
 
 class Conf(object):
     def __init__(self):
-        self.winfredHomeDir = os.path.expandvars("$HOME/.winfred")
+        self.os = detect_os()
+
+        if self.os == OsPlatform.Linux:
+            self.mainTextFontSize = 24
+            self.userProfileDir = os.path.expandvars("$HOME")
+        elif self.os == OsPlatform.Windows:
+            self.mainTextFontSize = 28
+            self.userProfileDir = os.path.expandvars("%userprofile%")
+        elif self.os == OsPlatform.macOS:
+            self.mainTextFontSize = 42
+            self.userProfileDir = os.path.expandvars("$HOME")
+
+        self.winfredHomeDir = os.path.join(self.userProfileDir, ".winfred")
         if not os.path.exists(self.winfredHomeDir):
             os.mkdir(self.winfredHomeDir)
 
         self.confFilePath = os.path.join(self.winfredHomeDir, "winfred.conf")
-
         self.logFilePath = os.path.join(self.winfredHomeDir, "winfred.log")
         logging.basicConfig(filename=self.logFilePath, level=logging.DEBUG,
                             format='%(levelname)s %(asctime)s [%(filename)s:%(lineno)d]%(message)s')
 
-        self.os = detect_os()
         logging.info("Conf init, platform detected: %s", self.os)
+        logging.info("Conf init, winfred HOME: %s", self.winfredHomeDir)
 
-        if self.os == OsPlatform.Linux:
-            self.mainTextFontSize = 24
-        elif self.os == OsPlatform.Windows:
-            self.mainTextFontSize = 28
-        elif self.os == OsPlatform.macOS:
-            self.mainTextFontSize = 42
--- Winfred/MainWindow.py
+++ Winfred/MainWindow.py
@@ -1,6 +1,7 @@
 from PyQt6.QtWidgets import QMainWindow
 from PyQt6.QtCore import Qt, QMargins, QPointF
 from PyQt6.QtGui import QShortcut, QKeySequence
+from pynput import keyboard
 
 from .MainText import MainText
 
@@ -19,6 +20,9 @@ class WinfredMainWindow(QMainWindow):
         edit = MainText(conf.mainTextFontSize)
         self.setContentsMargins(QMargins(6, 2, 6, 2))
         self.setCentralWidget(edit)
+
+        self.mainHotKeyListener = keyboard.GlobalHotKeys({"<ctrl>+y": self.show})
+        self.mainHotKeyListener.start()
 
     def mousePressEvent(self, event):
         self.oldPos = event.globalPosition()
