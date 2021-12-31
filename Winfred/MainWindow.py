3691171c912a71ddf68d3e37ed7824c63db560d1
tevenfeng <fengdingwen@outlook.com>
2021-12-31 11:17:56 +0800

    refactor

--- Winfred/MainWindow.py
+++ Winfred/MainWindow.py
@@ -9,26 +9,29 @@ from .MainText import MainText
 class WinfredMainWindow(QMainWindow):
     def __init__(self, conf):
         super(WinfredMainWindow, self).__init__()
+        self.mainEdit = None
+        self.oldPos = self.pos()
+        self.initUI(conf)
+
+        QShortcut(QKeySequence(Qt.Key.Key_Escape), self, self.hide)
+        self.mainHotKeyListener = keyboard.GlobalHotKeys({"<ctrl>+y": self.show})
+        self.mainHotKeyListener.start()
+
+    def initUI(self, conf):
         self.setWindowTitle("Winfred")
         self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.setFixedSize(700, 64)
         self.centerOnScreen()
         self.setStyleSheet("background-color: black;")
-        QShortcut(QKeySequence(Qt.Key.Key_Escape), self, self.hide)
 
-        self.oldPos = self.pos()
-
-        edit = MainText(conf.mainTextFontSize)
+        self.mainEdit = MainText(conf.mainTextFontSize)
         self.setContentsMargins(QMargins(6, 0, 6, 0))
-        self.setCentralWidget(edit)
-
-        self.mainHotKeyListener = keyboard.GlobalHotKeys({"<ctrl>+y": self.show})
-        self.mainHotKeyListener.start()
+        self.setCentralWidget(self.mainEdit)
 
     def centerOnScreen(self):
         resolution = QGuiApplication.primaryScreen().availableGeometry()
         self.move((resolution.width() / 2) - (self.frameSize().width() / 2),
-                  (resolution.height() / 2) - (self.frameSize().height() / 2))
+                  (resolution.height() / 3) - (self.frameSize().height() / 2))
 
     def mousePressEvent(self, event):
         self.oldPos = event.globalPosition()
